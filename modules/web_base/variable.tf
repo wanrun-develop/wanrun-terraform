378af683aea91152876af3ed0104092dcc63d274
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-09-01 21:33:26 +0900

    create: web base in modules

---
+++ modules/web_base/variable.tf
@@ -0,0 +1,24 @@
+variable "service_name" {
+  type    = string
+  default = ""
+}
+
+variable "env" {
+  type    = string
+  default = ""
+}
+
+variable "web_front_domain" {
+  type = string
+  default = ""
+}
+
+variable "front_web_waf_acl_id" {
+  type = string
+  default = ""
+}
+
+variable "cloudfront_acm_arn" {
+  type = string
+  default = ""
+}
