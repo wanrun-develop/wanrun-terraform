f777f7e40a2765f041d37cd6020ff980f6621547
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-10-20 17:28:11 +0900

    create: data processing

---
+++ envs/develop/2.core/data.tf
@@ -0,0 +1,61 @@
+locals {
+  # vpc
+  vpc_name = "${var.service_name}-${var.env}-vpc"
+
+  # sg
+  sg_alb_name = "${var.service_name}-${var.env}-wanrun-alb-sg"
+}
+
+###########################################
+# data参照
+###########################################
+data "aws_vpc" "wanrun" {
+  filter {
+    name   = "tag:Name"
+    values = [local.vpc_name]
+  }
+}
+
+data "aws_subnets" "public" {
+}
+
+// VPC内の全サブネットを取得
+data "aws_subnets" "vpc" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.wanrun]
+  }
+}
+
+// Publicサブネットのみを取得
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.wanrun.id
+
+  filter {
+    name   = "tag:Tier"
+    values = ["Public"]
+  }
+}
+
+// Privateサブネットのみを取得
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.wanrun.id
+
+  filter {
+    name   = "tag:Tier"
+    values = ["Private"]
+  }
+}
+
+// alb sgの取得
+data "aws_security_group" "alb_sg" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.wanrun.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [local.sg_alb_name]
+  }
+}
