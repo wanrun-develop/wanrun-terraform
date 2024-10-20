73e4b74f2b21f953bee7fb51dbe7ff46b625e1f2
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-10-21 00:03:40 +0900

    create: s3 access log

---
+++ modules/ops/variable.tf
@@ -0,0 +1,14 @@
+variable "service_name" {
+  type    = string
+  default = ""
+}
+
+variable "env" {
+  type    = string
+  default = ""
+}
+
+variable "retention_period" {
+  type    = number
+  default = 365
+}
