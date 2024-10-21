aeb533a5519d3cac1871edbe6afe8c5d92aae7d6
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-10-22 00:06:35 +0900

    Feature/backend alb (#11)
    * create: wanrun alb
    
    * add: alb sg
    
    * create: data processing
    
    * add: 呼び出しもとフィールド
    
    * fix
    
    * fix

--- envs/develop/1.network/security_group.tf
+++ envs/develop/1.network/security_group.tf
@@ -34,3 +34,40 @@ resource "aws_vpc_security_group_ingress_rule" "vpc_endpoint_ingress" {
     Name = "${var.service_name}-${var.env}-vpc-endpoint-ingress"
   }
 }
+
+###########################################
+# ALB
+###########################################
+resource "aws_security_group" "wanrun_be" {
+  name        = "${var.service_name}-${var.env}-wanrun-alb-sg"
+  vpc_id      = aws_vpc.wanrun.id
+  description = "${var.service_name}-${var.env}-wanrun-alb-sg"
+
+  ingress {
+    prefix_list_ids = ["pl-58a04531"] // AWSが提供しているcloudfrontのプレフィックスリスト
+    description     = "Allow CloudFront IPs"
+    from_port       = "443"
+    protocol        = "tcp"
+    to_port         = "443"
+  }
+
+  ingress {
+    cidr_blocks = [var.vpc_cidr]
+    description = "Allow VPC in wanrun"
+    from_port   = "443"
+    protocol    = "tcp"
+    to_port     = "443"
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = "0"
+    protocol    = "-1"
+    self        = "false"
+    to_port     = "0"
+  }
+
+  tags = {
+    Name = "${var.service_name}-${var.env}-wanrun-alb-sg"
+  }
+}
--- envs/develop/2.core/module_core.tf
+++ envs/develop/2.core/module_core.tf
@@ -6,4 +6,13 @@ module "core" {
   #   web_front_domain = var.web_front_domain
   # front_web_waf_acl_id = 
   # cloudfront_acm_arn = 
+
+  alb_wanrun_sgs       = [data.aws_security_group.alb_sg.id]
+  public_subnets       = data.aws_subnets.public.ids
+  alb_wanrun_idle_time = 60
+  certificate_arn      = ""
+
+  // 本番だけ
+  alb_wanrun_access_log_bucket_id = ""
+  alb_wanrun_access_log_prefix    = ""
 }
