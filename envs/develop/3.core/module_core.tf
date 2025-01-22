4fe3726f7c0777914823223781320e034c3567ce
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-01-23 00:44:29 +0900

    fix

--- envs/develop/3.core/module_core.tf
+++ envs/develop/3.core/module_core.tf
@@ -6,17 +6,18 @@ module "core" {
   #   main_domain = var.web_front_domain
   # main_waf_acl_id = 
   # cloudfront_acm_arn = 
+  certificate_arn = ""
 
-  alb_wanrun_sgs       = [data.aws_security_group.alb_sg.id]
-  public_subnets       = data.aws_subnets.public.ids
-  alb_wanrun_idle_time = 60
-  certificate_arn      = ""
+  # alb
+  internal_gateway_security_groups = [data.aws_security_group.alb_sg.id]
+  private_subnet_ids               = data.aws_subnets.private.ids
+  alb_internal_gateway_idle_time   = 60
 
   // 本番のみ設定をする
-  alb_wanrun_access_log_bucket_id = ""
-  alb_wanrun_access_log_prefix    = ""
+  # alb_wanrun_access_log_bucket_id = ""
+  # alb_wanrun_access_log_prefix    = ""
 
-  // ecs cluster
+  # ecs cluster
   is_container_insights                          = false
   fargate_base_capacity_provider_strategy        = var.fargate_base_capacity_provider_strategy
   fargate_weight_capacity_provider_strategy      = var.fargate_weight_capacity_provider_strategy
@@ -32,4 +33,6 @@ module "core" {
   # gateway
   cloudfront_access_control_header_key   = "X-Origin-Access-Control"
   cloudfront_access_control_header_value = data.aws_ssm_parameter.cloudfront_access_control_header_value.value
+  whitelist_locations                    = ["JP"]                 // 許可する国指定
+  access_control_allow_origins           = ["https://wanrun.com"] // TODO: 購入したドメイン名
 }
