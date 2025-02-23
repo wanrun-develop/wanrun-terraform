9faf2b798fca965d5ba5ca7faa8d0a1c459da717
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-02-23 17:42:24 +0900

    add: ssh setting

--- misc/db.sh
+++ misc/db.sh
@@ -30,3 +30,17 @@ goenv --version
 goenv install 1.22.1
 goenv versions
 goenv global 1.22.1
+
+# ssh setting
+ssh-keygen -t rsa -b 4096 -C "ec2-github" -f ~/.ssh/github_deploy_key
+
+# ~/.ssh/configに下記を書き込み
+ Host github.com
+  IdentityFile ~/.ssh/github_deploy_key
+  User git
+
+# 接続確認
+ssh -T git@github.com
+
+cd /opt
+git clone git@github.com:wanrun-develop/wanrun-migrate.git
