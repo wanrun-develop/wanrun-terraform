03eb2dccbbcde21669b03ad374119f79ea47cfb6
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-02-23 16:35:37 +0900

    add: shell

--- misc/db.sh
+++ misc/db.sh
@@ -6,6 +6,27 @@ dnf update -y \
 && rm -rf /var/cache/dnf/
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
+# postgresql
 dnf install -y postgresql15-server
 
-sh-5.2$ sudo postgresql-setup initdb
+sudo postgresql-setup initdb
+
+sudo passwd postgres
+
+
+# goenv
+git clone https://github.com/syndbg/goenv.git ~/.goenv
+
+echo 'export GOENV_ROOT="$HOME/.goenv"' >> ~/.bashrc
+echo 'export PATH="$GOENV_ROOT/bin:$PATH"' >> ~/.bashrc
+
+echo 'export PATH="$GOROOT/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.bashrc
+
+echo 'eval "$(goenv init -)"' >> ~/.bashrc
+
+goenv --version
+
+goenv install 1.22.1
+goenv versions
+goenv global 1.22.1
