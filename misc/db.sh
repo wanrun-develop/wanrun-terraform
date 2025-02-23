d21cccf850344377bfb7ad88921fe7037c4c4a23
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-02-23 18:52:08 +0900

    add: ssm port forwarding

--- misc/db.sh
+++ misc/db.sh
@@ -44,3 +44,33 @@ ssh -T git@github.com
 
 cd /opt
 git clone git@github.com:wanrun-develop/wanrun-migrate.git
+
+
+aws ssm start-session \
+    --target i-0dc2ba19951e1da20 \
+    --document-name AWS-StartPortForwardingSession \
+    --parameters '{"portNumber":["5432"],"localPortNumber":["15432"]}'
+
+# pgadmin setting
+vim /var/lib/pgsql/data/pg_hba.conf
+```
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     md5
+
+# IPv4 local connections (Change "ident" to "md5")
+host    all             all             127.0.0.1/32            md5
+
+# IPv6 local connections (Change "ident" to "md5")
+host    all             all             ::1/128                 md5
+
+# Allow replication connections from localhost, by a user with the
+# replication privilege.
+local   replication     all                                     md5
+host    replication     all             127.0.0.1/32            md5
+host    replication     all             ::1/128                 md5
+
+# Allow connections from any source (Required for SSM Port Forwarding)
+host    all             all             0.0.0.0/0               md5
+```
